b9e349f17f90557b69a0e9e18a817d7aea988dd9
mradul458 <72191359+mradul458@users.noreply.github.com>
2020-10-01 15:19:40 +0530

    Update check-if-added.php
    there was an incorrect table name user_items

--- check-if-added.php
+++ check-if-added.php
@@ -3,7 +3,7 @@ function check_if_added_to_cart($item_id)
 {
     $user_id=$_SESSION['id'];
     include("includes/common.php");
-    $select_query="SELECT * FROM user_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
+    $select_query="SELECT * FROM users_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
     $select_query_result=mysqli_query($con,$select_query) or die(mysqli_error($con));
     $check=mysqli_num_rows($select_query_result);
     if($check>=1)
